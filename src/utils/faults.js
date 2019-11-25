d71b05dfa2d86d3094d5f9526d07d7d756204853
jgilbert01 <jgilbert01@yahoo.com>
2019-11-24 22:39:01 -0500

    added fault handling and utils

---
+++ src/utils/faults.js
@@ -0,0 +1,23 @@
+import Promise from 'bluebird';
+
+export const throwFault = (uow) => (err) => {
+  // adorn the troubled uow
+  // for processing in the errors handler
+  err.uow = uow;
+  throw err;
+};
+
+export const rejectWithFault = (uow) => (err) => {
+  // adorn the troubled uow
+  // for processing in the errors handler
+  err.uow = uow;
+  return Promise.reject(err);
+};
+
+export const faulty = (funct) => (uow) => { // eslint-disable-line consistent-return
+  try {
+    return funct(uow);
+  } catch (e) {
+    throwFault(uow)(e);
+  }
+};
---
+++ test/unit/faults/index.test.js
@@ -0,0 +1,105 @@
+import 'mocha';
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import { fromKinesis, Publisher, toKinesisRecords } from '../../../src';
+import { faults, flushFaults, FAULT_EVENT_TYPE } from '../../../src/faults';
+
+let publishStub;
+
+describe('faults/index.js', () => {
+  beforeEach(() => {
+    publishStub = sinon.stub(Publisher.prototype, 'publish').resolves({});
+  });
+
+  afterEach(sinon.restore);
+
+  it('should handled logic error', (done) => {
+    const simulateHandledError = (uow) => {
+      if (uow.record.kinesis.sequenceNumber === '1') {
+        const e = new Error('handled error, has uow attached');
+        e.uow = uow;
+        throw e;
+      } else {
+        return uow;
+      }
+    };
+
+    const events = toKinesisRecords([
+      {
+        type: 'f1',
+      },
+      {
+        type: 'f2',
+      },
+      {
+        type: 'f3',
+      },
+    ]);
+
+    fromKinesis(events)
+      .map(simulateHandledError)
+      .errors(faults)
+      .through(flushFaults)
+
+      .collect()
+      .tap((collected) => {
+        // console.log(JSON.stringify(collected, null, 2));
+
+        expect(publishStub.calledOnce);
+
+        expect(collected.length).to.equal(3);
+
+        expect(collected[2].type).to.equal(FAULT_EVENT_TYPE);
+        expect(collected[2].err.name).to.equal('Error');
+        expect(collected[2].err.message).to.equal('handled error, has uow attached');
+
+        expect(collected[2].tags).to.deep.equal({
+          functionname: 'undefined',
+          pipeline: 'undefined',
+        });
+
+        expect(collected[2].uow).to.deep.equal({
+          record: {
+            eventSource: 'aws:kinesis',
+            eventID: 'shardId-000000000000:1',
+            awsRegion: 'us-west-2',
+            kinesis: {
+              sequenceNumber: '1',
+              data: 'eyJ0eXBlIjoiZjIifQ==',
+            },
+          },
+          event: {
+            id: 'shardId-000000000000:1',
+            type: 'f2',
+          },
+        });
+      })
+      .done(done);
+  });
+
+  it('should account for unhandled logic error', (done) => {
+    const spy = sinon.spy();
+    const err = new Error('unhandled error, no uow attached');
+    const simulateUnhandledError = (uow) => {
+      throw err;
+    };
+
+    const events = toKinesisRecords([
+      {
+        type: 'u1',
+      },
+    ]);
+
+    fromKinesis(events)
+      .map(simulateUnhandledError)
+      .errors(faults)
+      .stopOnError(spy)
+      .collect()
+      .tap((collected) => {
+        expect(spy).to.have.been.calledWith(err);
+        expect(collected.length).to.equal(0);
+      })
+      .done(done);
+  });
+});
