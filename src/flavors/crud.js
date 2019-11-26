1d16b38f9ba1071b52d9f025323e6f80883e140b
jgilbert01 <jgilbert01@yahoo.com>
2019-11-26 15:49:42 -0500

    added crud pipeline

---
+++ src/flavors/crud.js
@@ -0,0 +1,41 @@
+import {
+  printStartPipeline, printEndPipeline, debug,
+  faulty, faultyAsync,
+  toBatchUow, unBatchUow, publishEvents,
+} from '../utils';
+
+import { filterOnEventType, filterOnContent } from '../filters';
+
+export const crud = (rule) => (s) => s // eslint-disable-line import/prefer-default-export
+  .filter(onEventType(rule))
+  .tap(printStartPipeline(rule.id))
+
+  .filter(onContent(rule))
+
+  .map(toEvent(rule))
+  .parallel(rule.parallel || Number(process.env.PARALLEL) || 4)
+
+  .batch(rule.batchSize || Number(process.env.PUBLISH_BATCH_SIZE) || 25)
+  .map(toBatchUow)
+  .map(publish(rule))
+  .parallel(rule.parallel || Number(process.env.PARALLEL) || 4)
+
+  .flatMap(unBatchUow)
+  .tap(printEndPipeline(rule.id));
+
+const onEventType = (rule) => faulty((uow) => filterOnEventType(rule, uow));
+const onContent = (rule) => faulty((uow) => filterOnContent(rule, uow));
+
+const toEvent = (rule) => faultyAsync((uow) =>
+  (!rule.toEvent ? Promise.resolve(uow)
+    : Promise.resolve(rule.toEvent(uow, rule))
+      .then((event) => ({
+        ...uow,
+        event: {
+          ...uow.event,
+          ...event,
+        },
+      }))));
+
+const publish = (rule) =>
+  publishEvents(debug(rule.id), rule.streamName || process.env.STREAM_NAME);
---
+++ test/unit/flavors/crud.test.js
@@ -0,0 +1,111 @@
+import 'mocha';
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import {
+  initialize, execute, initializeFrom,
+  toDynamodbRecords, fromDynamodb,
+  Publisher,
+} from '../../../src';
+
+import { crud } from '../../../src/flavors/crud';
+
+describe('flavors/crud.js', () => {
+  beforeEach(() => {
+    initialize({
+      ...initializeFrom(rules),
+    });
+
+    sinon.stub(Publisher.prototype, 'publish').resolves({});
+  });
+
+  afterEach(sinon.restore);
+
+  it('should execute', (done) => {
+    const events = toDynamodbRecords([
+      {
+        timestamp: 1572832690,
+        keys: {
+          hk: '1',
+          sk: 'thing',
+        },
+        newImage: {
+          hk: '1',
+          sk: 'thing',
+          discriminator: 'thing',
+          name: 'Thing One',
+          description: 'This is thing one',
+          ttl: 1549053422,
+          timestamp: 1548967022000,
+        },
+      },
+      {
+        timestamp: 1572832690,
+        keys: {
+          hk: '1',
+          sk: 'other',
+        },
+        newImage: {
+          hk: '1',
+          sk: 'other',
+          discriminator: 'other',
+          name: 'Other One',
+          description: 'This is other one',
+          ttl: 1549053422,
+          timestamp: 1548967022000,
+        },
+      },
+    ]);
+
+    execute(fromDynamodb(events), false)
+      .collect()
+      // .tap((collected) => console.log(JSON.stringify(collected, null, 2)))
+      .tap((collected) => {
+        expect(collected.length).to.equal(2);
+        expect(collected[0].pipeline).to.equal('crud1');
+        expect(collected[0].event.type).to.equal('thing-created');
+        expect(collected[0].event.thing).to.deep.equal({
+          id: '1',
+          name: 'Thing One',
+          description: 'This is thing one',
+        });
+        expect(collected[0].event.tags).to.deep.equal({
+          region: 'us-west-2',
+          field1: 'v1',
+        });
+      })
+      .done(done);
+  });
+});
+
+const toEvent = (uow) => ({
+  thing: {
+    id: uow.event.raw.new.hk,
+    name: uow.event.raw.new.name,
+    description: uow.event.raw.new.description,
+  },
+  tags: {
+    ...uow.event.tags,
+    field1: 'v1',
+  },
+});
+
+const rules = [
+  {
+    id: 'crud1',
+    pipeline: crud,
+    eventType: /thing-*/,
+    filters: [() => true],
+    toEvent,
+  },
+  {
+    id: 'crud2',
+    pipeline: crud,
+    eventType: /other-*/,
+  },
+  {
+    id: 'crud-other1',
+    pipeline: crud,
+    eventType: 'x9',
+  },
+];
