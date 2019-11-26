01cecf9b339426642db43f98507f5842d3aca9da
jgilbert01 <jgilbert01@yahoo.com>
2019-11-26 14:37:07 -0500

    added filters

---
+++ test/unit/filter/index.test.js
@@ -0,0 +1,78 @@
+import 'mocha';
+import { expect } from 'chai';
+
+import { filterOnEventType, filterOnContent } from '../../../src/filters';
+
+describe('filters', () => {
+  it('should filter by type', () => {
+    const rule = {
+      eventType: 't1',
+    };
+
+    expect(filterOnEventType(rule, { event: { type: 't1' } })).to.be.true;
+    expect(filterOnEventType(rule, { event: { type: 't2' } })).to.be.false;
+  });
+
+  it('should filter by regex', () => {
+    const rule = {
+      eventType: /t.*/,
+    };
+
+    expect(filterOnEventType(rule, { event: { type: 't1' } })).to.be.true;
+    expect(filterOnEventType(rule, { event: { type: 't3' } })).to.be.true;
+    expect(filterOnEventType(rule, { event: { type: 'x1' } })).to.be.false;
+  });
+
+  it('should filter by array of types', () => {
+    const rule = {
+      eventType: ['t1', 't2'],
+    };
+
+    expect(filterOnEventType(rule, { event: { type: 't1' } })).to.be.true;
+    expect(filterOnEventType(rule, { event: { type: 't2' } })).to.be.true;
+    expect(filterOnEventType(rule, { event: { type: 't3' } })).to.be.false;
+  });
+
+  it('should filter by function', () => {
+    const rule = {
+      eventType: (type) => type.startsWith('t'),
+    };
+
+    expect(filterOnEventType(rule, { event: { type: 't1' } })).to.be.true;
+    expect(filterOnEventType(rule, { event: { type: 'x1' } })).to.be.false;
+  });
+
+  it('should throw', () => {
+    const rule = {
+      eventType: false,
+    };
+
+    expect(() => filterOnEventType(rule, {})).to.throw('Rule: undefined, has improperly configured eventType filter. Must be a string, array of string, regex or function.');
+  });
+
+  it('should filter on content', () => {
+    const rule = {
+      filters: [
+        (uow, r) => uow.event.tags.f === r.value,
+      ],
+      value: 'v1',
+    };
+
+    expect(filterOnContent(rule, { event: { tags: { f: 'v1' } } })).to.be.true;
+    expect(filterOnContent(rule, { event: { tags: { f: 'v2' } } })).to.be.false;
+  });
+
+  it('should filter on compound content', () => {
+    const rule = {
+      filters: [
+        (uow, r) => uow.event.tags.f1 === r.value,
+        (uow, r) => uow.event.tags.f2 === r.value,
+      ],
+      value: 'v1',
+    };
+
+    expect(filterOnContent(rule, { event: { tags: { f1: 'v1', f2: 'v1' } } })).to.be.true;
+    expect(filterOnContent(rule, { event: { tags: { f1: 'v2', f2: 'v1' } } })).to.be.false;
+    expect(filterOnContent(rule, { event: { tags: { f1: 'v1', f2: 'v2' } } })).to.be.false;
+  });
+});
