38b7913766fb13a46b51a2968d919fc8a08051e4
jgilbert01 <jgilbert01@yahoo.com>
2020-07-22 13:00:52 -0400

    added sqs connector

---
+++ src/connectors/sqs.js
@@ -0,0 +1,36 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+import { SQS, config } from 'aws-sdk';
+import Promise from 'bluebird';
+
+config.setPromisesDependency(Promise);
+
+class Connector {
+  constructor({
+    debug,
+    queueUrl = process.env.QUEUE_URL,
+    timeout = process.env.SQS_TIMEOUT || process.env.TIMEOUT || 1000,
+  }) {
+    this.debug = debug;
+    this.queueUrl = queueUrl || 'undefined';
+    this.queue = new SQS({
+      httpOptions: {
+        timeout,
+        // agent: sslAgent,
+      },
+      logger: { log: /* istanbul ignore next */ (msg) => this.debug(msg) },
+    });
+  }
+
+  sendMessageBatch(inputParams) {
+    const params = {
+      QueueUrl: this.queueUrl,
+      ...inputParams,
+    };
+
+    return this.queue.sendMessageBatch(params).promise()
+      .tap(this.debug)
+      .tapCatch(this.debug);
+  }
+}
+
+export default Connector;
