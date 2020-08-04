eb2df77e3ea8efc8e1b34afac884721b6150ddeb
jgilbert01 <jgilbert01@yahoo.com>
2020-08-04 05:37:29 -0400

    clean up

--- src/utils/tags.js
+++ src/utils/tags.js
@@ -2,7 +2,7 @@ import { skipTag } from '../filters';
 
 export const adornStandardTags = (eventField) => (uow) => ({
   ...uow,
-  event: {
+  [eventField]: {
     ...uow[eventField],
     tags: {
       ...envTags(uow.pipeline),
@@ -15,8 +15,8 @@ export const adornStandardTags = (eventField) => (uow) => ({
 export const envTags = (pipeline) => ({
   account: process.env.ACCOUNT_NAME || 'undefined',
   region: process.env.AWS_REGION || /* istanbul ignore next */ 'undefined',
-  stage: process.env.SERVERLESS_STAGE || 'undefined',
-  source: process.env.SERVERLESS_PROJECT || 'undefined',
+  stage: process.env.STAGE || process.env.SERVERLESS_STAGE || 'undefined',
+  source: process.env.PROJECT || process.env.SERVERLESS_PROJECT || 'undefined',
   functionname: process.env.AWS_LAMBDA_FUNCTION_NAME || 'undefined',
   pipeline: pipeline || 'undefined',
 });
