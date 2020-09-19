a92b42475cff8e77cf944b508ab0be5647921b31
jgilbert01 <jgilbert01@yahoo.com>
2020-09-19 11:00:22 -0400

    added rest int tests

---
+++ templates/rest-bff-service/test/int/rest/index.test.js
@@ -0,0 +1,44 @@
+import 'mocha';
+import { expect } from 'chai';
+
+const supertest = require('supertest');
+
+const endpoint = process.env.ENDPOINT ? process.env.ENDPOINT : 'http://localhost:3001';
+const client = supertest(endpoint);
+
+const THING = {
+  name: 'thing0',
+  description: 'This is thing zero.',
+};
+
+describe('rest/index.js', () => {
+  it('should save', () => client.put('/v1/things/00000000-0000-0000-0000-000000000000')
+    .send(THING)
+    // .set('Authorization', JWT)
+    .expect(200)
+    .expect((res) => {
+      // console.log('RES: %s', JSON.stringify(res, null, 2));
+      expect(JSON.parse(res.text)).to.deep.equal({});
+    }));
+
+  it('should get', () => client.get('/v1/things/00000000-0000-0000-0000-000000000000')
+    // .set('Authorization', JWT)
+    .expect(200)
+    .expect((res) => {
+      // console.log('RES: %s', JSON.stringify(res, null, 2));
+      expect(JSON.parse(res.text)).to.deep.equal([{
+        id: '00000000-0000-0000-0000-000000000000',
+        lastModifiedBy: 'offlineContext_authorizer_principalId',
+        timestamp: 1600349040394,
+        ...THING,
+      }]);
+    }));
+
+  it('should delete', () => client.delete('/v1/things/00000000-0000-0000-0000-000000000000')
+    // .set('Authorization', JWT)
+    .expect(200)
+    .expect((res) => {
+      // console.log('RES: %s', JSON.stringify(res, null, 2));
+      expect(JSON.parse(res.text)).to.deep.equal({});
+    }));
+});
