8cc65c67d9458e1a8cbb52380eb43948e57d0b84
jgilbert01 <jgilbert01@yahoo.com>
2020-09-19 17:20:10 -0400

    more int test

--- templates/rest-bff-service/src/rest/routes/thing.js
+++ templates/rest-bff-service/src/rest/routes/thing.js
@@ -4,7 +4,7 @@ export const getThing = (req, res) => get(req.namespace, req.params.id)
   .then((data) => res.status(200).json(data));
 
 export const saveThing = (req, res) => save(req.namespace, req.params.id, req.body)
-  .then((data) => res.status(200).json(data));
+  .then((data) => res.status(200).json({}));
 
 export const deleteThing = (req, res) => del(req.namespace, req.params.id)
-  .then((data) => res.status(200).json(data));
+  .then((data) => res.status(200).json({}));
