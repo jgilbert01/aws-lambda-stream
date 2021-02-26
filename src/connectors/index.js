68d0936f982938bd9effc66a8226d1d7baf74bfe
John Gilbert <1082126+jgilbert01@users.noreply.github.com>
2021-02-26 12:34:15 -0500

    add-sec-mgr-support (#74)
    * incremented version
    
    * added secrets mgr
    
    * init debug

--- src/connectors/index.js
+++ src/connectors/index.js
@@ -2,6 +2,7 @@ export { default as DynamoDBConnector } from './dynamodb';
 export { default as EventBridgeConnector } from './eventbridge';
 export { default as FirehoseConnector } from './firehose';
 export { default as KinesisConnector } from './kinesis';
+export { default as SecretsMgrConnector } from './secretsmgr';
 export { default as S3Connector } from './s3';
 export { default as SnsConnector } from './sns';
 export { default as SqsConnector } from './sqs';
