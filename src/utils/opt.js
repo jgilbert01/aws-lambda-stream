855e14541c807836bc8ef17bb20bcb3613a91aeb
John Gilbert <john.c.gilbert@associates.ice.dhs.gov>
2021-02-26 12:25:15 -0500

    init debug

--- src/utils/opt.js
+++ src/utils/opt.js
@@ -1,6 +1,9 @@
 import { publishToEventBridge } from './eventbridge';
 
+import { debug } from './print';
+
 export const defaultOptions = { // eslint-disable-line import/prefer-default-export
+  debug: debug('handler'),
   busName: process.env.BUS_NAME,
   publish: publishToEventBridge,
 };
