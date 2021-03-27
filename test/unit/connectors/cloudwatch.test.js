fa3f76a1c325be9d5562633cdcc963b7998f9333
John Gilbert <1082126+jgilbert01@users.noreply.github.com>
2021-03-27 07:05:26 -0400

    added cloudwatch connector

---
+++ test/unit/connectors/cloudwatch.test.js
@@ -0,0 +1,58 @@
+import 'mocha';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import debug from 'debug';
+import AWS from 'aws-sdk-mock';
+import Promise from 'bluebird';
+
+import Connector from '../../../src/connectors/cloudwatch';
+
+AWS.Promise = Promise;
+
+describe('connectors/cloudwatch.js', () => {
+  afterEach(() => {
+    AWS.restore('CloudWatch');
+  });
+
+  it('should put', async () => {
+    const spy = sinon.spy((params, cb) => cb(null, {}));
+    AWS.mock('CloudWatch', 'putMetricData', spy);
+
+    const MetricData = [{
+      MetricName: 'domain.event',
+      Unit: 'Count',
+      Value: 1,
+      Dimensions: [
+        {
+          Name: 'account',
+          Value: 'dev',
+        }, {
+          Name: 'region',
+          Value: 'us-east-1',
+        }, {
+          Name: 'source',
+          Value: 'service-x',
+        }, {
+          Name: 'function',
+          Value: 'f1',
+        }, {
+          Name: 'pipeline',
+          Value: 'p1',
+        }, {
+          Name: 'type',
+          Value: 'thing-created',
+        },
+      ],
+    }];
+
+    const data = await new Connector({ debug: debug('cw') })
+      .put({ Namespace: 'ns', MetricData });
+
+    expect(spy).to.have.been.calledOnce;
+    expect(spy).to.have.been.calledWith({
+      Namespace: 'ns',
+      MetricData,
+    });
+    expect(data).to.deep.equal({});
+  });
+});
