cc3015792ef1af9a717c2e151db649b327d0fdb1
Jay Merrifield <John.W.Merrifield@associates.ice.dhs.gov>
2021-09-17 14:04:24 -0400

    Added support for cognito events

---
+++ src/from/cognito.js
@@ -0,0 +1,33 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+import _ from 'highland';
+import * as uuid from 'uuid';
+import kebabCase from 'lodash/kebabCase';
+
+import { faulty } from '../utils';
+
+export const fromCognito = (event, { eventTypePrefix = 'aws-cognito' } = {}) => // eslint-disable-line import/prefer-default-export
+
+  // event: https://docs.aws.amazon.com/cognito/latest/developerguide/cognito-user-identity-pools-working-with-aws-lambda-triggers.html#cognito-user-pools-lambda-trigger-event-parameter-shared
+  _([{
+    // create a unit-of-work for the single event
+    // create a stream to work with the rest of the framework
+    record: event,
+  }])
+    .map(faulty((uow) =>
+      // create a unit-of-work for each event
+      // so we can correlate related work for error handling
+      ({
+        ...uow,
+        event: {
+          id: uuid.v4(),
+          type: `${eventTypePrefix}-${kebabCase(uow.record.triggerSource)}`,
+          partitionKey: uow.record.userName,
+          tags: {
+            region: uow.record.region,
+            source: uow.record.userPoolId,
+          },
+          raw: {
+            ...uow.record.request,
+          },
+        },
+      })));
---
+++ test/unit/from/cognito.test.js
@@ -0,0 +1,65 @@
+import 'mocha';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import * as uuid from 'uuid';
+
+import { fromCognito } from '../../../src/from/cognito';
+
+const postConfirmationEvent = {
+  version: '1',
+  triggerSource: 'PostConfirmation_ConfirmSignUp',
+  region: process.env.AWS_REGION,
+  userPoolId: 'us-west-2_WvvYlc2XP',
+  userName: '5e44e08e-4b70-4eac-b710-2b86fbeddad8',
+  callerContext: {
+    awsSdkVersion: '1.2.123',
+    clientId: '3gd7kk5ml13s821fi8uimm73mu',
+  },
+  request: {
+    userAttributes: {
+      sub: '091c283f-61b0-47aa-916d-bec63211d363',
+      given_name: 'FAKEY',
+      family_name: 'MCFAKERSON',
+      email: 'fakey.mcfakerson@email.com',
+    },
+  },
+  response: {},
+};
+
+describe('from/cognito.js', () => {
+  beforeEach(() => {
+    sinon.stub(uuid, 'v4').returns('221c5dbf-e314-4c75-97b5-f47c46f6b171');
+  });
+
+  afterEach(sinon.restore);
+  it('should parse records', (done) => {
+    fromCognito(postConfirmationEvent)
+      .collect()
+      .tap((collected) => {
+        // console.log(JSON.stringify(collected, null, 2));
+
+        expect(collected.length).to.equal(1);
+        expect(collected[0]).to.deep.equal({
+          record: postConfirmationEvent,
+          event: {
+            id: '221c5dbf-e314-4c75-97b5-f47c46f6b171',
+            type: 'aws-cognito-post-confirmation-confirm-sign-up',
+            partitionKey: '5e44e08e-4b70-4eac-b710-2b86fbeddad8',
+            tags: {
+              region: 'us-west-2',
+              source: 'us-west-2_WvvYlc2XP',
+            },
+            raw: {
+              userAttributes: {
+                email: 'fakey.mcfakerson@email.com',
+                family_name: 'MCFAKERSON',
+                given_name: 'FAKEY',
+                sub: '091c283f-61b0-47aa-916d-bec63211d363',
+              },
+            },
+          },
+        });
+      })
+      .done(done);
+  });
+});
