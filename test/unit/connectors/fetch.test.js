4fc60c1a27bcf80472a4609765e6a23a8c3057d2
Jay Merrifield <John.W.Merrifield@associates.ice.dhs.gov>
2021-10-01 15:44:02 -0400

    Added status code check to fetch connector

--- test/unit/connectors/fetch.test.js
+++ test/unit/connectors/fetch.test.js
@@ -2,6 +2,7 @@ import 'mocha';
 import { expect } from 'chai';
 import debug from 'debug';
 import fetchMock from 'fetch-mock';
+import { FetchError } from 'node-fetch';
 
 import Connector from '../../../src/connectors/fetch';
 
@@ -24,6 +25,45 @@ describe('connectors/fetch.js', () => {
     expect(data).to.deep.equal({
       ...RESPONSE,
     });
+  });
+  it('should reject on 500 server error', async () => {
+    fetchMock.get(url, 500);
+
+    const connector = new Connector({ debug: debug('fetch'), timeout: 3000 });
+
+    try {
+      await connector.fetch(url, { method: 'GET' });
+    } catch (e) {
+      expect(e).to.be.instanceOf(FetchError);
+      return;
+    }
+    throw new Error('fetch did not throw an error');
+  });
+  it('should reject on 400 server error', async () => {
+    fetchMock.get(url, 400);
+
+    const connector = new Connector({ debug: debug('fetch'), timeout: 3000 });
+
+    try {
+      await connector.fetch(url, { method: 'GET' });
+    } catch (e) {
+      expect(e).to.be.instanceOf(FetchError);
+      return;
+    }
+    throw new Error('fetch did not throw an error');
+  });
+  it('should reject on 404 server error', async () => {
+    fetchMock.get(url, 404);
+
+    const connector = new Connector({ debug: debug('fetch'), timeout: 3000 });
+
+    try {
+      await connector.fetch(url, { method: 'GET' });
+    } catch (e) {
+      expect(e).to.be.instanceOf(FetchError);
+      return;
+    }
+    throw new Error('fetch did not throw an error');
   });
 });
 
