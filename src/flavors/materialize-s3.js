e4e0ea50ad8dbdfaa15327dde35cb8a0790e4fd1
John Gilbert <john.gilbert@devtechnology.com>
2022-12-17 14:30:24 -0500

    add-materialize-s3

---
+++ src/flavors/materialize-s3.js
@@ -0,0 +1,37 @@
+import {
+  printStartPipeline, printEndPipeline,
+  faulty, faultyAsyncStream, faultify,
+   putObjectToS3, splitObject,
+  } from '../utils';
+
+  import {
+    filterOnEventType, filterOnContent,
+    outSkip, outSourceIsSelf,
+  } from '../filters';
+  
+  
+export const materializeS3 = (rule) => (s) => s // eslint-disable-line import/prefer-default-export
+  .filter(outSkip)
+  // .filter(outSourceIsSelf)
+
+  .filter(onEventType(rule))
+  .tap(printStartPipeline)
+
+  .filter(onContent(rule))
+
+  .through(splitObject(rule))
+
+  .map(toPutRequest(rule))
+  .parallel(rule.parallel || Number(process.env.PARALLEL) || 4)
+
+  .through(putObjectToS3(rule))
+
+  .tap(printEndPipeline);
+
+const onEventType = (rule) => faulty((uow) => filterOnEventType(rule, uow));
+const onContent = (rule) => faulty((uow) => filterOnContent(rule, uow));
+
+const toPutRequest = (rule) => faultyAsyncStream(async (uow) => ({
+  ...uow,
+  putRequest: await faultify(rule.toPutRequest)(uow, rule),
+}));
