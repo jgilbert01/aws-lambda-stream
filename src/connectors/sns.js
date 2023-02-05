2e072687871600a7086508c628e5fb35f1c9030a
John Gilbert <john.gilbert@devtechnology.com>
2023-02-05 15:24:42 -0500

    add-batch-retry-support sns

--- src/connectors/sns.js
+++ src/connectors/sns.js
@@ -2,6 +2,10 @@
 import { SNS, config } from 'aws-sdk';
 import Promise from 'bluebird';
 
+import {
+  defaultRetryConfig, wait, getDelay, assertMaxRetries,
+} from '../utils/retry';
+
 config.setPromisesDependency(Promise);
 
 class Connector {
@@ -9,15 +13,20 @@ class Connector {
     debug,
     topicArn = process.env.TOPIC_ARN,
     timeout = Number(process.env.SNS_TIMEOUT) || Number(process.env.TIMEOUT) || 1000,
+    retryConfig = defaultRetryConfig,
   }) {
     this.debug = (msg) => debug('%j', msg);
     this.topicArn = topicArn || 'undefined';
     this.topic = new SNS({
       httpOptions: {
         timeout,
+        connectTimeout: timeout,
       },
+      maxRetries: 10, // Default: 3
+      retryDelayOptions: { base: 200 }, // Default: 100 ms
       logger: { log: /* istanbul ignore next */ (msg) => debug('%s', msg.replace(/\n/g, '\r')) },
     });
+    this.retryConfig = retryConfig;
   }
 
   publish(inputParams) {
@@ -26,10 +35,51 @@ class Connector {
       ...inputParams,
     };
 
-    return this.topic.publish(params).promise()
+    return this.topic.publish(params)
+      .promise()
       .tap(this.debug)
       .tapCatch(this.debug);
+  }
+
+  publishBatch(inputParams) {
+    const params = {
+      TopicArn: this.topicArn,
+      ...inputParams,
+    };
+
+    return this._publishBatch(params, []);
+  }
+
+  _publishBatch(params, attempts) {
+    assertMaxRetries(attempts, this.retryConfig.maxRetries);
+
+    return wait(getDelay(this.retryConfig.retryWait, attempts.length))
+      .then(() => this.topic.publishBatch(params)
+        .promise()
+        .tap(this.debug)
+        .tapCatch(this.debug)
+        .then((resp) => {
+          if (resp.Failed?.length > 0) {
+            return this._publishBatch(unprocessed(params, resp), [...attempts, resp]);
+          } else {
+            return accumlate(attempts, resp);
+          }
+        }));
   }
 }
 
 export default Connector;
+
+export const unprocessed = (params, resp) => ({
+  ...params,
+  PublishBatchRequestEntries: resp.Failed.map((m) => params.PublishBatchRequestEntries.find((e) => m.Id === e.Id)),
+});
+
+export const accumlate = (attempts, resp) => attempts.reduceRight((a, c) => ({
+  ...a,
+  Successful: [
+    ...c.Successful,
+    ...a.Successful,
+  ],
+  attempts: [...attempts, resp],
+}), resp);
--- test/unit/connectors/sns.test.js
+++ test/unit/connectors/sns.test.js
@@ -7,12 +7,12 @@ import Connector from '../../../src/connectors/sns';
 
 import { debug } from '../../../src/utils';
 
-describe('connectors/sns.js', () => {
+describe.only('connectors/sns.js', () => {
   afterEach(() => {
     AWS.restore('SNS');
   });
 
-  it('should send msg', async () => {
+  it('should publish msg', async () => {
     const spy = sinon.spy((params, cb) => cb(null, {}));
     AWS.mock('SNS', 'publish', spy);
 
@@ -30,5 +30,147 @@ describe('connectors/sns.js', () => {
       Message: inputParams.Message,
     });
     expect(data).to.deep.equal({});
+  });
+
+  it('should publish batch msg', async () => {
+    const spy = sinon.spy((params, cb) => cb(null, { Successful: [{ Id: '1' }] }));
+    AWS.mock('SNS', 'publishBatch', spy);
+
+    const inputParams = {
+      PublishBatchRequestEntries: [
+        {
+          Id: '1',
+          Message: JSON.stringify({ f1: 'v1' }),
+        },
+      ],
+    };
+
+    const data = await new Connector({
+      debug: debug('sqs'),
+      topicArn: 't1',
+    }).publishBatch(inputParams);
+
+    expect(spy).to.have.been.calledWith({
+      TopicArn: 't1',
+      PublishBatchRequestEntries: inputParams.PublishBatchRequestEntries,
+    });
+    expect(data).to.deep.equal({ Successful: [{ Id: '1' }] });
+  });
+
+  it('should retry', async () => {
+    const responses = [
+      { Successful: [{ Id: '1' }], Failed: [{ Id: '2' }, { Id: '3' }] },
+      { Successful: [{ Id: '2' }], Failed: [{ Id: '3' }] },
+      { Successful: [{ Id: '3' }] },
+    ];
+
+    const spy = sinon.spy((params, cb) => cb(null, responses.shift()));
+    AWS.mock('SNS', 'publishBatch', spy);
+
+    const inputParams = {
+      PublishBatchRequestEntries: [
+        {
+          Id: '1',
+          Message: JSON.stringify({ f1: 'v1' }),
+        },
+        {
+          Id: '2',
+          Message: JSON.stringify({ f2: 'v2' }),
+        },
+        {
+          Id: '3',
+          Message: JSON.stringify({ f3: 'v3' }),
+        },
+      ],
+    };
+
+    const data = await new Connector({
+      debug: debug('sqs'),
+      topicArn: 't1',
+    }).publishBatch(inputParams);
+
+    expect(spy).to.have.been.calledWith({
+      PublishBatchRequestEntries: [inputParams.PublishBatchRequestEntries[0], inputParams.PublishBatchRequestEntries[1], inputParams.PublishBatchRequestEntries[2]],
+      TopicArn: 't1',
+    });
+    expect(spy).to.have.been.calledWith({
+      PublishBatchRequestEntries: [inputParams.PublishBatchRequestEntries[1], inputParams.PublishBatchRequestEntries[2]],
+      TopicArn: 't1',
+    });
+    expect(spy).to.have.been.calledWith({
+      PublishBatchRequestEntries: [inputParams.PublishBatchRequestEntries[2]],
+      TopicArn: 't1',
+    });
+
+    expect(data).to.deep.equal({
+      Successful: [{ Id: '1' }, { Id: '2' }, { Id: '3' }],
+      attempts: [
+        {
+          Successful: [{ Id: '1' }],
+          Failed: [{ Id: '2' }, { Id: '3' }],
+        },
+        {
+          Successful: [{ Id: '2' }],
+          Failed: [{ Id: '3' }],
+        },
+        {
+          Successful: [{ Id: '3' }],
+        },
+      ],
+    });
+  });
+
+  it('should throw on max retry', async () => {
+    const responses = [
+      { Successful: [{ Id: '1' }], Failed: [{ Id: '2' }, { Id: '3' }] },
+      { Successful: [{ Id: '2' }], Failed: [{ Id: '3' }] },
+    ];
+
+    const spy = sinon.spy((params, cb) => cb(null, responses.shift()));
+    AWS.mock('SNS', 'publishBatch', spy);
+
+    const inputParams = {
+      PublishBatchRequestEntries: [
+        {
+          Id: '1',
+          Message: JSON.stringify({ f1: 'v1' }),
+        },
+        {
+          Id: '2',
+          Message: JSON.stringify({ f2: 'v2' }),
+        },
+        {
+          Id: '3',
+          Message: JSON.stringify({ f3: 'v3' }),
+        },
+      ],
+    };
+
+    const data = await new Connector({
+      debug: debug('sqs'),
+      topicArn: 't1',
+      retryConfig: {
+        maxRetries: 1,
+        retryWait: 100,
+      },
+    }).publishBatch(inputParams)
+      .then(() => {
+        expect.fail('should have thrown');
+      }).catch((err) => {
+        expect(spy).to.have.been.calledWith({
+          PublishBatchRequestEntries: [inputParams.PublishBatchRequestEntries[0], inputParams.PublishBatchRequestEntries[1], inputParams.PublishBatchRequestEntries[2]],
+          TopicArn: 't1',
+        });
+        expect(spy).to.have.been.calledWith({
+          PublishBatchRequestEntries: [inputParams.PublishBatchRequestEntries[1], inputParams.PublishBatchRequestEntries[2]],
+          TopicArn: 't1',
+        });
+        expect(spy).to.not.have.been.calledWith({
+          PublishBatchRequestEntries: [inputParams.PublishBatchRequestEntries[2]],
+          TopicArn: 't1',
+        });
+
+        expect(err.message).to.contain('Failed batch requests');
+      });
   });
 });
