afff9d9027916dfbe7fbe919f3422fa6e29b358f
John Gilbert <john.gilbert@devtechnology.com>
2023-02-05 03:39:42 -0500

    add-batch-retry-support eb

--- src/connectors/eventbridge.js
+++ src/connectors/eventbridge.js
@@ -2,27 +2,65 @@
 import { EventBridge, config } from 'aws-sdk';
 import Promise from 'bluebird';
 
+import {
+  defaultRetryConfig, wait, getDelay, assertMaxRetries,
+} from '../utils/retry';
+
 config.setPromisesDependency(Promise);
 
 class Connector {
   constructor({
     debug,
     timeout = Number(process.env.BUS_TIMEOUT) || Number(process.env.TIMEOUT) || 1000,
+    retryConfig = defaultRetryConfig,
   }) {
     this.debug = (msg) => debug('%j', msg);
     this.bus = new EventBridge({
       httpOptions: {
         timeout,
+        connectTimeout: timeout,
       },
+      maxRetries: 10, // Default: 3
+      retryDelayOptions: { base: 200 }, // Default: 100 ms
       logger: { log: /* istanbul ignore next */ (msg) => debug('%s', msg.replace(/\n/g, '\r')) },
     });
+    this.retryConfig = retryConfig;
   }
 
   putEvents(params) {
-    return this.bus.putEvents(params).promise()
-      .tap(this.debug)
-      .tapCatch(this.debug);
+    return this._putEvents(params, []);
+  }
+
+  _putEvents(params, attempts) {
+    assertMaxRetries(attempts, this.retryConfig.maxRetries);
+
+    return wait(getDelay(this.retryConfig.retryWait, attempts.length))
+      .then(() => this.bus.putEvents(params)
+        .promise()
+        .tap(this.debug)
+        .tapCatch(this.debug)
+        .then((resp) => {
+          if (resp.FailedEntryCount > 0) {
+            return this._putEvents(unprocessed(params, resp), [...attempts, resp]);
+          } else {
+            return accumlate(attempts, resp);
+          }
+        }));
   }
 }
 
 export default Connector;
+
+export const unprocessed = (params, resp) => ({
+  ...params,
+  Entries: params.Entries.filter((e, i) => resp.Entries[i].ErrorCode),
+});
+
+export const accumlate = (attempts, resp) => attempts.reduceRight((a, c) => ({
+  ...a,
+  Entries: [
+    ...c.Entries.filter((e) => !e.ErrorCode),
+    ...a.Entries.filter((e) => !e.ErrorCode),
+  ],
+  attempts: [...attempts, resp],
+}), resp);
--- test/unit/connectors/eventbridge.test.js
+++ test/unit/connectors/eventbridge.test.js
@@ -13,7 +13,7 @@ describe('connectors/eventbridge.js', () => {
   });
 
   it('should publish', async () => {
-    const spy = sinon.spy((params, cb) => cb(null, {}));
+    const spy = sinon.spy((params, cb) => cb(null, { Entries: [{ EventId: '1' }], FailedEntryCount: 0 }));
     AWS.mock('EventBridge', 'putEvents', spy);
 
     const inputParams = {
@@ -33,6 +33,107 @@ describe('connectors/eventbridge.js', () => {
     expect(spy).to.have.been.calledWith({
       Entries: inputParams.Entries,
     });
-    expect(data).to.deep.equal({});
+    expect(data).to.deep.equal({ Entries: [{ EventId: '1' }], FailedEntryCount: 0 });
+  });
+
+  it('should retry', async () => {
+    const responses = [
+      { Entries: [{ EventId: '1' }, { ErrorCode: 'X' }, { ErrorCode: 'X' }], FailedEntryCount: 2 },
+      { Entries: [{ EventId: '2' }, { ErrorCode: 'X' }], FailedEntryCount: 1 },
+      { Entries: [{ EventId: '3' }], FailedEntryCount: 0 },
+    ];
+
+    const spy = sinon.spy((params, cb) => cb(null, responses.shift()));
+    AWS.mock('EventBridge', 'putEvents', spy);
+
+    const inputParams = {
+      Entries: [
+        {
+          EventBusName: 'b1',
+          DetailType: 't1',
+          Detail: JSON.stringify({ type: 't1' }),
+        },
+        {
+          EventBusName: 'b1',
+          DetailType: 't2',
+          Detail: JSON.stringify({ type: 't2' }),
+        },
+        {
+          EventBusName: 'b1',
+          DetailType: 't3',
+          Detail: JSON.stringify({ type: 't3' }),
+        },
+      ],
+    };
+
+    const data = await new Connector({
+      debug: debug('eventbridge'),
+    }).putEvents(inputParams);
+
+    expect(spy).to.have.been.calledWith({
+      Entries: inputParams.Entries,
+    });
+    expect(data).to.deep.equal({
+      Entries: [{ EventId: '1' }, { EventId: '2' }, { EventId: '3' }],
+      FailedEntryCount: 0,
+      attempts: [
+        {
+          Entries: [{ EventId: '1' }, { ErrorCode: 'X' }, { ErrorCode: 'X' }],
+          FailedEntryCount: 2,
+        },
+        {
+          Entries: [{ EventId: '2' }, { ErrorCode: 'X' }],
+          FailedEntryCount: 1,
+        },
+        {
+          Entries: [{ EventId: '3' }],
+          FailedEntryCount: 0,
+        },
+      ],
+    });
+  });
+
+  it('should throw on max retry', async () => {
+    const responses = [
+      { Entries: [{ EventId: '1' }, { ErrorCode: 'X' }, { ErrorCode: 'X' }], FailedEntryCount: 2 },
+      { Entries: [{ EventId: '2' }, { ErrorCode: 'X' }], FailedEntryCount: 1 },
+      { Entries: [{ EventId: '3' }], FailedEntryCount: 0 },
+    ];
+
+    const spy = sinon.spy((params, cb) => cb(null, responses.shift()));
+    AWS.mock('EventBridge', 'putEvents', spy);
+
+    const inputParams = {
+      Entries: [
+        {
+          EventBusName: 'b1',
+          DetailType: 't1',
+          Detail: JSON.stringify({ type: 't1' }),
+        },
+        {
+          EventBusName: 'b1',
+          DetailType: 't2',
+          Detail: JSON.stringify({ type: 't2' }),
+        },
+        {
+          EventBusName: 'b1',
+          DetailType: 't3',
+          Detail: JSON.stringify({ type: 't3' }),
+        },
+      ],
+    };
+
+    const data = await new Connector({
+      debug: debug('eventbridge'),
+      retryConfig: {
+        maxRetries: 1,
+        retryWait: 100,
+      },
+    }).putEvents(inputParams)
+      .then(() => {
+        expect.fail('should have thrown');
+      }).catch((err) => {
+        expect(err.message).to.contain('Failed batch requests');
+      });
   });
 });
