c7e9df0e52d0c3fb8aba40c006e0198d1373ed6c
John Gilbert <john.gilbert@devtechnology.com>
2024-03-08 15:19:44 -0500

    added retry backoff

--- src/utils/lambda-test.js
+++ src/utils/lambda-test.js
@@ -1,6 +1,7 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 import { InvokeCommand, LambdaClient } from '@aws-sdk/client-lambda';
 import { NodeHttpHandler } from '@smithy/node-http-handler';
+import { ConfiguredRetryStrategy } from '@smithy/util-retry';
 
 import { defaultDebugLogger } from './log';
 import { debug as d } from './print';
@@ -8,7 +9,9 @@ import { debug as d } from './print';
 export default ({
   functionName,
   endpoint = 'http://localhost:3002',
-  timeout = 1000,
+  timeout = 6000,
+  maxAttempt = 8,
+  retryDelay = 1000,
   debug = d('test'),
 
 }) => {
@@ -18,6 +21,11 @@ export default ({
       requestTimeout: timeout,
       connectionTimeout: timeout,
     }),
+    retryStrategy: new ConfiguredRetryStrategy(
+      maxAttempt,
+      // istanbul ignore next
+      (attempt) => 100 + attempt * retryDelay,
+    ), // backoff function for when serverless-offline is slow to start up
     logger: defaultDebugLogger(debug),
   });
 
