c909bda8cf739d7b3c0ca4859c95f9db1a0b9cd4
John Gilbert <john.gilbert@devtechnology.com>
2024-09-20 17:02:51 -0400

    tests

--- test/unit/sinks/claimcheck.test.js
+++ test/unit/sinks/claimcheck.test.js
@@ -12,54 +12,6 @@ import Connector from '../../../src/connectors/s3';
 describe('sinks/claimcheck.js', () => {
   afterEach(sinon.restore);
 
-  it('should store event', (done) => {
-    const uows = [
-      {}, // not too large
-      { // too large
-        putClaimcheckRequest: {
-          Bucket: 'b1',
-          Key: 'us-west-2/claimchecks/2024/09/21/12/1',
-          Body: JSON.stringify({ id: '1' }),
-        },
-      },
-      {}, // not too large
-    ];
-
-    const stub = sinon.stub(Connector.prototype, 'putObject').resolves({});
-
-    _(uows)
-      .batch() // batch in
-      .through(storeClaimcheck({
-        claimCheckBucketName: 'event-lake-s3',
-      }))
-      .collect()
-      .tap((collected) => {
-        // console.log(JSON.stringify(collected, null, 2));
-
-        expect(collected.length).to.equal(1); // assert batch out
-
-        expect(stub).to.have.been.calledWith({
-          Bucket: 'b1',
-          Key: 'us-west-2/claimchecks/2024/09/21/12/1',
-          Body: JSON.stringify({ id: '1' }),
-        });
-
-        expect(collected[0]).to.deep.equal([
-          {},
-          {
-            putClaimcheckRequest: {
-              Bucket: 'b1',
-              Key: 'us-west-2/claimchecks/2024/09/21/12/1',
-              Body: JSON.stringify({ id: '1' }),
-            },
-            putClaimcheckResponse: {},
-          },
-          {},
-        ]);
-      })
-      .done(done);
-  });
-
   it('should handle oversized requests', (done) => {
     sinon.stub(time, 'now').returns(new Date(1726854864001));
     const spy = sinon.spy();
@@ -116,6 +68,54 @@ describe('sinks/claimcheck.js', () => {
               Body: '{\"id\":\"3\",\"body\":\"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\"}',
             },
           },
+        ]);
+      })
+      .done(done);
+  });
+
+  it('should store event', (done) => {
+    const uows = [
+      {}, // not too large
+      { // too large
+        putClaimcheckRequest: {
+          Bucket: 'b1',
+          Key: 'us-west-2/claimchecks/2024/09/21/12/1',
+          Body: JSON.stringify({ id: '1' }),
+        },
+      },
+      {}, // not too large
+    ];
+
+    const stub = sinon.stub(Connector.prototype, 'putObject').resolves({});
+
+    _(uows)
+      .batch() // batch in
+      .through(storeClaimcheck({
+        claimCheckBucketName: 'event-lake-s3',
+      }))
+      .collect()
+      .tap((collected) => {
+        // console.log(JSON.stringify(collected, null, 2));
+
+        expect(collected.length).to.equal(1); // assert batch out
+
+        expect(stub).to.have.been.calledWith({
+          Bucket: 'b1',
+          Key: 'us-west-2/claimchecks/2024/09/21/12/1',
+          Body: JSON.stringify({ id: '1' }),
+        });
+
+        expect(collected[0]).to.deep.equal([
+          {},
+          {
+            putClaimcheckRequest: {
+              Bucket: 'b1',
+              Key: 'us-west-2/claimchecks/2024/09/21/12/1',
+              Body: JSON.stringify({ id: '1' }),
+            },
+            putClaimcheckResponse: {},
+          },
+          {},
         ]);
       })
       .done(done);
